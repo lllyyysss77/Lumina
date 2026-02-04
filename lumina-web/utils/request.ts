a65ca6c4abd317e731e9d652e0f102f128ed7abc
jojo <1911572936@qq.com>
2026-02-04 13:17:08 +0800

    chore: 修改前端 API 基础路径为本地地址
    主要改动：
    - 将 baseURL 从生产环境地址改为本地开发地址
    - 从 https://lumina.jojoz.cn/api/v1 改为 http://127.0.0.1:8080/api/v1
    - 方便本地开发和测试

--- lumina-web/utils/request.ts
+++ lumina-web/utils/request.ts
@@ -187,7 +187,7 @@ class HttpClient {
 
 // Export a singleton instance with default configuration
 export const api = new HttpClient({
-  baseURL: 'https://lumina.jojoz.cn/api/v1',
+  baseURL: 'http://127.0.0.1:8080/api/v1',
 });
 
 export default HttpClient;
